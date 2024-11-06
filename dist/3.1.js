5214aacc9a804b87c6f0af3d3debe46ed3982266
ayan-akd <ayankumar.akd@gmail.com>
2024-11-06 21:24:34 +0600

    Watched first lesson

--- dist/3.1.js
+++ dist/3.1.js
@@ -1,2 +1,27 @@
 "use strict";
-console.log("checking");
+{
+    //OOP - Class
+    class Animal {
+        // public name: string;
+        // public species: string;
+        // public sound: string;
+        //parameter properties
+        constructor(animalName, animalSpecies, animalSound) {
+            this.animalName = animalName;
+            this.animalSpecies = animalSpecies;
+            this.animalSound = animalSound;
+            // this.name = animalName;
+            // this.species = animalSpecies;
+            // this.sound = animalSound;
+        }
+        makeSound() {
+            console.log(`The ${this.animalName} makes ${this.animalSound} sound.`);
+        }
+    }
+    const dog = new Animal("Local Dog", "Dog", "Vou Vou");
+    const cat = new Animal("Local Cat", "Cat", "Meow Meow");
+    const bird = new Animal("Local Bird", "Bird", "Tweet Tweet");
+    dog.makeSound();
+    cat.makeSound();
+    bird.makeSound();
+}
--- src/3.1.ts
+++ src/3.1.ts
@@ -1,1 +1,32 @@
-console.log("checking");
+{
+  //OOP - Class
+
+  class Animal {
+    // public name: string;
+    // public species: string;
+    // public sound: string;
+
+    //parameter properties
+
+    constructor(
+      public animalName: string,
+      public animalSpecies: string,
+      public animalSound: string
+    ) {
+      // this.name = animalName;
+      // this.species = animalSpecies;
+      // this.sound = animalSound;
+    }
+
+    makeSound() {
+      console.log(`The ${this.animalName} makes ${this.animalSound} sound.`);
+    }
+  }
+
+  const dog = new Animal("Local Dog", "Dog", "Vou Vou");
+  const cat = new Animal("Local Cat", "Cat", "Meow Meow");
+  const bird = new Animal("Local Bird", "Bird", "Tweet Tweet");
+  dog.makeSound();
+  cat.makeSound();
+  bird.makeSound();
+}
